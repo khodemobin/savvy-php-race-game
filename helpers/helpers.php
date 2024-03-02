826ee3b7fc7be29b9055d1fb09197492148864ca
Mobin Yazdanparast <mobinyazdanparast9@gmail.com>
2024-03-03 01:57:37 +0330

    add dd() helper for easier debugging;

---
+++ helpers/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Component\VarDumper\VarDumper;
+
+if (!function_exists('dd')) {
+    function dd(mixed ...$vars)
+    {
+        if (!\in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) && !headers_sent()) {
+            header('HTTP/1.1 500 Internal Server Error');
+        }
+
+        if (array_key_exists(0, $vars) && 1 === count($vars)) {
+            VarDumper::dump($vars[0]);
+        } else {
+            foreach ($vars as $k => $v) {
+                VarDumper::dump($v, is_int($k) ? 1 + $k : $k);
+            }
+        }
+
+        exit(1);
+    }
+}
